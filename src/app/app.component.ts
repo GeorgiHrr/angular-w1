a8d49d168804980ef3664c91fc39c2c5c6deabef
GeorgiHr <gehr1235@gmail.com>
2024-04-25 14:25:30 +0300

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'hello-world-2001261061';
+
+  public buttonTextAndInfoPanel = [
+    {
+      text:false , visibility: false
+    },
+    {
+      text:false , visibility: false
+    },
+    {
+      text:false , visibility: false
+    },
+    {
+      text:false , visibility: false
+    },
+    {
+      text:false , visibility: false
+    },
+  ];
+
+  public isButtonClicked(show:number){
+
+    this.buttonTextAndInfoPanel[show].text = !this.buttonTextAndInfoPanel[show].text;
+    this.buttonTextAndInfoPanel[show].visibility = !this.buttonTextAndInfoPanel[show].visibility;
+
+  }
+
 }
